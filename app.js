c8d4935f8bae5dc1499b58939cbadfcc2db7a6a7
Unsorted-array <i_am_ritesh@outlook.com>
2017-10-14 14:12:22 +0530

    styling and db

--- app.js
+++ app.js
@@ -1,13 +1,42 @@
 var express = require('express');
 var app = express();
 var bodyparser = require('body-parser');
+var mongoose = require('mongoose');
 
 app.use(bodyparser.urlencoded({extended : true}))
 
 var locations =[ {name : "Manali" ,img : "https://farm5.staticflickr.com/4085/4837609363_0ec4a8c9b3.jpg"},
-    {name : "kullu" ,img : "https://farm1.staticflickr.com/748/21623620690_b8bf9051a3.jpg"},
+    ,
+    {name : "leh" ,img : "https://farm8.staticflickr.com/7356/9669339783_6fd63cef04.jpg"},
+    {name : "kullu" ,img : "https://farm4.staticflickr.com/3872/14435096036_39db8f04bc.jpg"},
+    {name : "leh" ,img : "https://farm8.staticflickr.com/7356/9669339783_6fd63cef04.jpg"},
+    {name : "kullu" ,img : "https://farm4.staticflickr.com/3872/14435096036_39db8f04bc.jpg"},
+    {name : "kullu" ,img : "https://farm4.staticflickr.com/3872/14435096036_39db8f04bc.jpg"},
+    {name : "leh" ,img : "https://farm8.staticflickr.com/7356/9669339783_6fd63cef04.jpg"},
     {name : "leh" ,img : "https://farm8.staticflickr.com/7356/9669339783_6fd63cef04.jpg"}
 ]
+
+mongoose.connect("mongodb://localhost/travelon",{useMongoClient:true});
+// schema setup
+
+var locationsSchema= new mongoose.Schema({
+    name: String,
+    img: String
+})
+
+var locations = mongoose.model("locations",locationsSchema);
+
+
+locations.create({name : "kullu" ,img : "https://farm4.staticflickr.com/3872/14435096036_39db8f04bc.jpg"},function (err, locations) {
+    if(err)
+    {
+        console.log(err)
+    }
+    else {
+        console.log(locations);
+    }
+
+});
 
 
 app.set('view engine','ejs')
--- readme.md
+++ readme.md
@@ -11,9 +11,19 @@
   *Add in bootstrap
 
 
-#creatin new locations
+# creatin new locations
 
-  *setup new campground Post route
-  *Add in body parser
-  *setup route to show form
-  *add basic style
+  * setup new campground Post route
+  * Add in body parser
+  * setup route to show form
+  * add basic style
+
+# style the Navbar and form
+
+  * Add a navbar to all templates
+  * style theb form page
+# Add mongoose
+
+  * Install and configure mongoose
+  * Setup location model
+  * use the model to the project
